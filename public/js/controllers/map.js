c4a13063e7b093e6c1174967848a07165b4d13ef
Adam Berro <adamberro@gmail.com>
2016-12-15 02:28:58 -0800

    style google map

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -36,7 +36,8 @@
     $scope.initMap = function() {
       $scope.map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 34.0312450, lng: -118.2665320},
-        zoom: 11
+        zoom: 11,
+        styles: [{"elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#0F0919"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#FDCF6D"}]},{"elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#002FA7"}]},{"featureType":"poi.attraction","elementType":"geometry.fill","stylers":[{"color":"#E60003"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#FBFCF4"}]},{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"color":"#23D366"}]},{"featureType":"poi.government","elementType":"geometry.fill","stylers":[{"color":"#D41C1D"}]},{"featureType":"poi.school","elementType":"geometry.fill","stylers":[{"color":"#C57BBB"}]},{"featureType":"transit.line","elementType":"geometry.fill","stylers":[{"saturation":-100}]}]
       });
       var infoWindow = new google.maps.InfoWindow({map: $scope.map});
 
@@ -84,7 +85,7 @@
           Math.random() * ($scope.bounds.f.b - $scope.bounds.f.f) + $scope.bounds.f.f,
           Math.random() * ($scope.bounds.b.b - $scope.bounds.b.f) + $scope.bounds.b.f
           ];
-        var contentString = '<h4><a href="http://twitter.com/'+ tweet.name + '/status/' + tweet.status_id +'" target="_blank">' + tweet.name + '</a></h4>' + '<p>' + tweet.text + '</p>' + '<p>Sentiment: ' + tweet.sentiment.score + '</p>';
+        var contentString = '<h4><a href="http://twitter.com/'+ tweet.name + '/status/' + tweet.status_id +'" target="_blank">' + tweet.name + '</a></h4>' + '<p>' + tweet.text + '</p>' + '<p><b>Sentiment</b>: ' + tweet.sentiment.score + '</p>';
         var infowindow = new google.maps.InfoWindow({
           content: contentString
         });
@@ -123,7 +124,7 @@
       })
     }
 
-    $scope.text = 'Enter search term';
+    $scope.text = 'bitsbytesLA1';
     $scope.submit = function() {
       return $scope.getTweets($scope.text)
         .then(function() {
@@ -133,17 +134,6 @@
         //   return $scope.addMarkers();
         // })
       }
-
-    $scope.sumSentiment = function() {
-      var sum = 0;
-      for (var i = 0; i < $scope.tweets; i++) {
-        sum += $scope.tweets[i].sentiment.score;
-      }
-      console.log(sum / $scope.tweets.length);
-      return sum / $scope.tweets.length;
-    }
-
-    $scope.sumSentiment()
 
   }
 
